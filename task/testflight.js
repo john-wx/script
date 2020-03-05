d00b676c97d6c20974a7722bcd3b277be4d852a4
John <gv.31909@gmail.com>
2020-03-05 21:52:45 +0800

    updat

--- task/testflight.js
+++ task/testflight.js
@@ -137,10 +137,10 @@ const url = "https://testflight.apple.com/join/";
  * 填入要监测的appkey。从testfligt地址获取。
  * 例如"VCIvwk2g,wArXdacJ,2vnRvOTX,LzjySbQx,IdFRwmNy,qDkBu2ur,4Qt2lIm5,ZzqOu8tX,ftCqFe6D,fy7LvHVA,QKqitFwc"
 */
-const appkey = "23LA2tmX,ZzqOu8tX,4nss4sdk,5baxW8ue";
+const appkey = "4nss4sdk";
 
 //是否在没有tf位置的时候任然弹出通知，默认不弹出,防止过多无用通知。
-var isNOtify = true;
+var isNOtify = false;
 const fullstr = /(此 Beta 版本的测试员已满)|(此 Beta 版本目前不接受任何新测试员)/;
 const appnamereg = /<title>加入 Beta 版“(.+)” - TestFlight - Apple<\/title>/;
 var proarray = new Array();
