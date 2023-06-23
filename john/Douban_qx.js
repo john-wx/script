2d4a9b8e1abadca541f229d07f6dc738c460aef6
john lx <gv.31909@gmail.com>
2023-06-23 22:25:55 +0800

    Update

--- john/Douban_qx.js
+++ john/Douban_qx.js
@@ -50,7 +50,7 @@ async function douban_addons() {
         let mweb = [`<a href="https://www.cupfox.com/search?key=${title[1]}">\t<img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/Chb.png" height="26" width="26" style="vertical-align: text-top;" /></a>`]
         mweb.push(`<a href="https://dsxys.pro/sb/ke7nhZe3c1-.html?wd=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/Dsx.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
         mweb.push(`<a href="https://im1907.top/?jx=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/Jxw.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
-        mweb.push(`<a href="https://www.o8tv.com/vodsearch/-------------.html?wd=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/555.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
+        mweb.push(`<a href="https://555yy6.com/vodsearch/-------------.html?wd=${title[1]}&submit="><img src="https://raw.githubusercontent.com/githubdulong/Script/master/Images/555.png" height="26" width="26" style="vertical-align: text-top;" /></a>`)
         
     let douban_options = {
         url: `https://frodo.douban.com/api/v2/movie/${movieId[1]}?apiKey=0ac44ae016490db2204ce0a042db2916`,
